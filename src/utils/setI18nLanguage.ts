7bfc6fcbffefef078ced391d4444a3d89d48833c
joepeng <joe@hellotalk.com>
2023-09-25 17:55:17 +0800

    fix: 修改i18n函数设置文件

--- src/utils/setI18nLanguage.ts
+++ src/utils/setI18nLanguage.ts
@@ -1,45 +1,43 @@
-import type { Locale } from 'vue-i18n'
-import type { App } from 'vue'
-import { createI18n } from 'vue-i18n'
+import type { Locale } from 'vue-i18n';
+import type { App } from 'vue';
+import { createI18n } from 'vue-i18n';
 
 const i18n = createI18n({
   legacy: false,
   locale: '',
   messages: {},
-})
+});
 
 const localesMap = Object.fromEntries(
-  Object.entries(import.meta.glob('../locales/*.yml'))
-    .map(([path, loadLocale]) => [path.match(/([\w-]*)\.yml$/)?.[1], loadLocale]),
-) as Record<Locale, () => Promise<{ default: Record<string, string> }>>
+  Object.entries(import.meta.glob('../locales/*.yml')).map(([path, loadLocale]) => [
+    path.match(/([\w-]*)\.yml$/)?.[1],
+    loadLocale,
+  ]),
+) as Record<Locale, () => Promise<{ default: Record<string, string> }>>;
 
-export const availableLocales = Object.keys(localesMap)
+export const availableLocales = Object.keys(localesMap);
 
-const loadedLanguages: string[] = []
+const loadedLanguages: string[] = [];
 
 function setI18nLanguage(lang: Locale) {
-  i18n.global.locale.value = lang as any
-  if (typeof document !== 'undefined')
-    document.querySelector('html')?.setAttribute('lang', lang)
-  return lang
+  i18n.global.locale.value = lang as any;
+  if (typeof document !== 'undefined') document.querySelector('html')?.setAttribute('lang', lang);
+  return lang;
 }
 
 // 设置多语言
 export async function loadLanguageAsync(lang: string): Promise<Locale> {
-  if (i18n.global.locale.value === lang)
-    return setI18nLanguage(lang)
+  if (i18n.global.locale.value === lang) return setI18nLanguage(lang);
 
-  if (loadedLanguages.includes(lang))
-    return setI18nLanguage(lang)
+  if (loadedLanguages.includes(lang)) return setI18nLanguage(lang);
 
-  const messages = await localesMap[lang]()
-  i18n.global.setLocaleMessage(lang, messages.default)
-  loadedLanguages.push(lang)
-  return setI18nLanguage(lang)
+  const messages = await localesMap[lang]();
+  i18n.global.setLocaleMessage(lang, messages.default);
+  loadedLanguages.push(lang);
+  return setI18nLanguage(lang);
 }
 
 export function installI18n(app: App) {
-  app.use(i18n)
-  // eslint-disable-next-line ts/no-floating-promises
-  loadLanguageAsync('en')
+  app.use(i18n);
+  loadLanguageAsync('en');
 }
